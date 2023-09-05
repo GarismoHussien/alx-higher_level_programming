e9db0400a25edabb448666ac4d4cb63c9558ee30
GarismoHussien <hand2land@gmail.com>
2023-09-05 02:47:52 -0700

    T

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
-for i in range(ord('a'), ord('z') + 1):
-    if i != ord('q') and i != ord('e'):
-        print("{:c}".format(i), end="")
+#!/usr/bin/python3
+for number in range(0, 100):
+    if number == 99:
+        print("{}".format(number))
+    else:
+        print("{:02}".format(number), end=", ")
